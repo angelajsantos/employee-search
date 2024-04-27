61ed6792384afdcb664a0abd5d862cc76448c177
Angela Santos <ajsantos@cpp.edu>
2024-04-27 01:10:51 -0700

    adding employee header file, main file, and table

--- employee.h
+++ employee.h
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stddef.h> 
 
+//provides a foundation for working with employee data
+    //allows creation of instances of employee struct
+    //use pointes to manipulate or read data
 typedef struct { 
-    long number; 
-    char *name; 
-    char *phone; 
-    double salary; 
+    long number;            //number of employee         
+    char *name;             //name of employee
+    char *phone;            //phone number of employee
+    double salary;          //salary of employee
 } Employee, *PtrToEmployee;
+    //PtrToEmployee = a pointter to an employee struction
+    //    can be used to point to an instance of employee and modify its contents
+    //used instead of writing struct Employee *
 
-typedef const Employee *PtrToConstEmployee;
+typedef const Employee *PtrToConstEmployee; 
+    //PtrToConstEmployee = a pointer to a constant employee structure
+    //    can point to an instance but prevents modification of the pointed-to-data
+
--- employeeMain.c
+++ employeeMain.c
@@ -6,22 +6,30 @@
 #include "employee.h" 
 int main(void) { 
 
-    //defined in employeeSearchOne.c 
+    //defined in employeeOne.c 
+    //function prototypes are declared within the main function, indicating that these functions are defined in an external file
+    // also wrappers
     PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind); 
     PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind); 
     PtrToEmployee searchEmployeeByPhone(const Employee table[], int sizeTable, char * phoneToFind);
     PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double salaryToFind);  
+        //const Employee table[] = a pointer (const Employee*) to the first element of an array of employee structures
 
     //defined in employeeTable.c 
     extern Employee EmployeeTable[];
     extern const int EmployeeTableEntries;
+        //external variables defined in another file, in this case employeeTable.c
     PtrToEmployee matchPtr; //Declaration 
+
 
     //searching for number
     //Example Found
     matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1001); 
+        //matchPtr will equal null if target element is not found due to the function
     if (matchPtr != NULL) 
         printf("Employee ID 1001 is in record %d\n", matchPtr - EmployeeTable); 
+            //prints index of the found employee in the employeetable array
+            //index is calculated by subtracting the base address of employeetable from the address stores in matchPtr
     else 
         printf("Employee ID 1045 is NOT found in the record\n"); 
     //Example not found
